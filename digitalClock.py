74c337acd7929cfbd21333f8a3ed37b4325f7567
Rajeev2604 <rajeevrc007@gmail.com>
2022-11-21 08:20:06 +0000

    Changed to Red Screen

--- digitalClock.py
+++ digitalClock.py
@@ -4,9 +4,9 @@ window = Tk()
 window.title("Digital Clock")
 
 window.geometry("600x300")
-window.configure(bg='steelblue')
+window.configure(bg='red')
 
-label = Label(window, text='Welcome', font=("Arial Black", 78, "bold"), bg='steelblue')
+label = Label(window, text='Welcome', font=("Arial Black", 78, "bold"), bg='red')
 label.pack(pady=100)
 
 window.mainloop()
