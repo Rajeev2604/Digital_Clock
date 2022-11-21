28ac38d02f141ec0dd7187cc42bd06eea5f13036
rajeevrc007 <simplyrk007@gmail.com>
2022-11-21 16:37:53 +0530

    Changes color to Dark Green

--- digitalClock.py
+++ digitalClock.py
@@ -4,9 +4,9 @@ window = Tk()
 window.title("Digital Clock")
 
 window.geometry("600x300")
-window.configure(bg='red')
+window.configure(bg='dark green')
 
-label = Label(window, text='Welcome', font=("Arial Black", 78, "bold"), bg='red')
+label = Label(window, text='Welcome', font=("Arial Black", 78, "bold"), bg='dark green')
 label.pack(pady=100)
 
 window.mainloop()
